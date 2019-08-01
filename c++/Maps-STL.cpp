91797f9e226e9f7de8570cfc0213ffc3a7239cda
sikandarkumar <sikandarkumardash@gmail.com>
2019-08-01 10:49:27 +0530

    stl in c++

--- c++/Maps-STL.cpp
+++ c++/Maps-STL.cpp
@@ -9,7 +9,31 @@ using namespace std;
 
 
 int main() {
+    int q;
+    std::map<string,int> marksheet;
+    cin>>q;
+    for(int i=0;i<q;i++){
+        int type,y;
+        string x;
+        cin>>type;
+        if(type==1){
+            cin>>x>>y;
+            marksheet[x] +=y;
+        }
+        if(type==2){
+            cin>>x;
+            marksheet.erase(x);
+        }
+        if(type==3){
+            cin>>x;
+            cout<<marksheet[x]<<endl;
+        }
+
+    }
        
     return 0;
 }
+
+
+
 
